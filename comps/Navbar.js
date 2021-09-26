4d724f04dc7d94fce2bb9fe9367db2155e5611e9
clear <harry.hareesha@adventhealth.com>
2021-09-26 15:33:41 -0700

    authready

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -4,7 +4,7 @@ import { useContext } from 'react'
 import { AuthContext } from '../stores/authContext'
 
 export default function Navbar() {
-    const { user, login, logout } = useContext(AuthContext)
+    const { user, login, logout, authReady } = useContext(AuthContext)
     console.log(user);
     return (
         <div className="container">
