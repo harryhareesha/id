9d13155fea81b7175b3f9adfb87cdc95524b0510
clear <harry.hareesha@adventhealth.com>
2021-09-26 14:18:19 -0700

    init

---
+++ pages/guides.js
@@ -0,0 +1,10 @@
+import styles from '../styles/Guides.module.css'
+
+export default function Guides() {
+  return (
+    <div className={styles.guides}>
+       <h2>Welcome to Gaming Vibes</h2>
+    
+    </div>
+  )
+}
